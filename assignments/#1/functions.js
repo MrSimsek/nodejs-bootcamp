f0f894dcb1b4bcf45eb5602b5d50060014f3719e
dnz <d>
2019-10-11 20:42:30 +0300

    Add homework

---
+++ assignments/#1/functions.js
@@ -0,0 +1,16 @@
+function introducer(name, age, job, livingLocation) {
+  // Use function parameters to produce the following output
+  // Example output: 'My name is Deniz. I am 26 years old. I am a full stack developer and I live in Istanbul.'
+}
+
+function calculateDogAge(age) {
+  // 1 human year is 7 dog years
+  // return the dog year for the given age parameter
+}
+
+function convertTurkishLira(amount, curreny) {
+  // currency can take 'dollar', 'euro' or 'pounds's
+  // convert given amount in Turkish lira to given currency
+  // Example usage:
+  // convertTurkishLira(6000, 'dollar') this will return 1019.30
+}
