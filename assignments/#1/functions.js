93cb20d7d90c5ef24f4c555f891a7c841051dedd
dnz <d>
2019-10-15 17:06:52 +0300

    Add assignment solutions

--- assignments/#1/functions.js
+++ assignments/#1/functions.js
@@ -1,11 +1,15 @@
 function introducer(name, age, job, livingLocation) {
   // Use function parameters to produce the following output
   // Example output: 'My name is Deniz. I am 26 years old. I am a full stack developer and I live in Istanbul.'
+  console.log(
+    `My name is ${name}. I am ${age} years old. I am a ${job} and I live in ${livingLocation}.`
+  );
 }
 
 function calculateDogAge(age) {
   // 1 human year is 7 dog years
   // return the dog year for the given age parameter
+  return age * 7;
 }
 
 function convertTurkishLira(amount, curreny) {
@@ -13,4 +17,18 @@ function convertTurkishLira(amount, curreny) {
   // convert given amount in Turkish lira to given currency
   // Example usage:
   // convertTurkishLira(6000, 'dollar') this will return 1019.30
+  
+  if (curreny === "dollar") {
+    return amount / 6;
+  }
+
+  if (curreny === "euro") {
+    return amount / 7;
+  }
+
+  if (curreny === "pounds") {
+    return amount / 8;
+  }
+
+  return "You are poor! Try again when you are rich...";
 }
